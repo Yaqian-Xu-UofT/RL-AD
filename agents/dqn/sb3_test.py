4c78cb0d2220661b946ff92de975976cb09eec08
user <81783344+dl423@users.noreply.github.com>
2025-11-27 10:16:21 -0500

    Use mps if available, rather than cpu

--- agents/dqn/sb3_test.py
+++ agents/dqn/sb3_test.py
@@ -1,26 +1,35 @@
 import gymnasium
 import highway_env
+import torch
 from stable_baselines3 import DQN
 
-env = gymnasium.make("highway-fast-v0", render_mode="human")
+# Detect device
+device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+print(f"Using device: {device}")
 
-# model = DQN('MlpPolicy', env,
-#               policy_kwargs=dict(net_arch=[256, 256]),
-#               learning_rate=5e-4,
-#               buffer_size=15000,
-#               learning_starts=200,
-#               batch_size=32,
-#               gamma=0.8,
-#               train_freq=1,
-#               gradient_steps=1,
-#               target_update_interval=50,
-#               verbose=1,
-#               tensorboard_log="highway_dqn/")
-# model.learn(int(2e4))
-# model.save("highway_dqn/model")
+# config = {"duration": 100,}
+
+env = gymnasium.make("highway-v0")
+# env = gymnasium.make("highway-v0", render_mode="human", config=config)
+
+model = DQN('MlpPolicy', env,
+              policy_kwargs=dict(net_arch=[256, 256]),
+              learning_rate=5e-4,
+              buffer_size=15000,
+              learning_starts=200,
+              batch_size=32,
+              gamma=0.8,
+              train_freq=1,
+              gradient_steps=1,
+              target_update_interval=50,
+              verbose=1,
+              tensorboard_log="results/logs/dqn/",
+              device=device)
+model.learn(int(1e5))
+model.save("results/models/dqn/")
 
 # Load and test saved model
-model = DQN.load("highway_dqn/model")
+model = DQN.load("results/models/dqn/", device=device)
 while True:
   done = truncated = False
   obs, info = env.reset()
